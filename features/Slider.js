b319ce5de638f5f377ccfa4a3580d2fe29a4fc3f
UssefBhl <ybhl83@gmail.com>
2022-05-13 10:29:14 +0100

    🔥 💄 Slider UI and functionality

---
+++ features/Slider.js
@@ -0,0 +1,16 @@
+import { StyleSheet, View, Text } from 'react-native';
+import { useFonts } from 'expo-font';
+
+export default function Slider({Component}) {
+    
+    return (
+        <View>
+            <Component />
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+
+});
