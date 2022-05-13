75b4bf4d1999b8b3894408a3aba6d92ea6fe8c3a
UssefBhl <ybhl83@gmail.com>
2022-05-13 10:28:47 +0100

    🔥 created Navbar UI and functionality

---
+++ features/Navbar.js
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import { StyleSheet, View, Pressable } from 'react-native';
+import { useFonts } from 'expo-font';
+import LogoLong from '../assets/icons/logoLong.svg';
+import SearchIcon from '../assets/icons/search.svg';
+import ProfileIcon from '../assets/icons/profile.svg';
+import HomeIcon from '../assets/icons/home.svg';
+import BasketIcon from '../assets/icons/basket.svg';
+
+
+export default function Navbar({slides, currentSlideIndex, goToSlide, setCurrentSlideIndex}) {
+
+
+    const changeNavigation = (index) => {
+        return () => {
+            goToSlide(index);
+            setCurrentSlideIndex(index)
+        }
+    }
+    // loading Poppins fonts
+    const [loaded] = useFonts({
+        'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    });
+
+    if (!loaded) {
+        return null;
+    }
+    
+    return (
+        <View style={styles.navbar}>    
+            <View style={styles.top_navbar}>
+                <LogoLong />
+                <View style={styles.search_icon} >
+                    <SearchIcon />
+                </View>
+            </View>
+
+            <View style={styles.bot_navbar} >
+                {slides.map((_,index) => (
+                    <Pressable onPress={changeNavigation(index)} style={{...styles.navbar_item , borderBottomWidth: currentSlideIndex === index ? 3 : 0}} >
+                        {
+                            index === 0 ?
+                                <HomeIcon/> :
+                            index === 1 ?
+                                <BasketIcon/>
+                            : <ProfileIcon/>
+                        }
+                    </Pressable>
+                ))}
+            </View>       
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    navbar: {
+        display: 'flex',
+        flexDirection: 'column',
+        paddingTop: 60,
+        backgroundColor: "#fff",
+        borderBottomColor: "#a9a9a95e",
+        borderBottomWidth: 1
+    },
+    top_navbar: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        paddingLeft: 10 ,
+        paddingRight: 10
+    },
+    search_icon: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#a9a9a95e',
+        width: 40,
+        height: 40,
+        padding: 5,
+        borderRadius: 999
+    },
+    bot_navbar: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginTop: 20
+    },
+    navbar_item: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: 10,
+        borderBottomColor: '#C69048',
+    }
+    
+});
