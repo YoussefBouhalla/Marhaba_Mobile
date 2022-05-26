a403699be92e2b915b1b9a786323b4c3f255ce86
UssefBhl <ybhl83@gmail.com>
2022-05-26 23:39:14 +0100

    🚧 meals services
    - seachForMeals
    - getMealImage

--- src/services/MealsServices.js
+++ src/services/MealsServices.js
@@ -1,11 +1,15 @@
 import axios from "axios"
 
 const AppURL = "http://192.168.137.1:4000/meals";
-
-export function searchMeals(data) {
-    return axios.post(`${AppURL}/search`, data);
+const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'authorization': '',
+}
+export function searchMeals(data, token) {
+    return axios.post(`${AppURL}/search`, data , {headers : {...headers, 'authorization' : `Bearer ${token}`}}, );
 }
 
 export function getMealImage(mealId) {
-    return axios.get(`${AppURL}/${mealId}`);
+    return axios.get(`${AppURL}/${mealId}`, {headers : {...headers, 'authorization' : `Bearer ${token}`}});
 }
